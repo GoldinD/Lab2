8cf7921411b7b420b8f698f0a560a6b820d030b1
Dean Goldin <1235950@students.wits.ac.za>
2019-04-20 12:27:30 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let path = require('path'); 
 let express = require('express'); 
 let app = express();
-let mainRouter = require('C:/Users/Dean/Desktop/Lab2/node_modules/express/lib/router/mainRoutes.js')
+let mainRouter = require('mainRoutes.js')
 
 // mainRouter.get('/', function (req, res) {
 //     res.send('Hello World'); 
