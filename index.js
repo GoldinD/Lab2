efebb44350c36dab3c48d69f9144b87e5130578a
Dean Goldin <1235950@students.wits.ac.za>
2019-04-20 13:07:39 +0200

    going back a step

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let path = require('path'); 
 let express = require('express'); 
 let app = express();
-let mainRouter = require('mainRoutes.js')
+let mainRouter = express.Router();
 
-// mainRouter.get('/', function (req, res) {
-//     res.send('Hello World'); 
-// });
-// mainRouter.get('/about', function(req, res){
-//     res.sendFile(path.join(__dirname, 'views', 'about.html'));
-// });
+mainRouter.get('/', function (req, res) {
+    res.send('Hello World'); 
+});
+mainRouter.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
 
 let port = process.env.PORT || 3000;
 app.listen(port);
