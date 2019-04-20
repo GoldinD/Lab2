5237c814aaa7701d5722374e9695973048af18f0
Dean Goldin <1235950@students.wits.ac.za>
2019-04-20 13:48:27 +0200

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ mainRouter.get('/', function (req, res) {
 mainRouter.get('/about', function(req, res){
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
-
+app.use(mainRouter);
 let port = process.env.PORT || 3000;
 app.listen(port);
 console.log("Express server running on port", port);
