34725446ceb663d8f2e5feb39ce5f6094f044784
Dean Goldin <1235950@students.wits.ac.za>
2019-04-20 12:08:10 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let path = require('path'); 
+let express = require('express'); 
+let app = express();
+let mainRouter = require('mainRoutes.js')
+
+// mainRouter.get('/', function (req, res) {
+//     res.send('Hello World'); 
+// });
+// mainRouter.get('/about', function(req, res){
+//     res.sendFile(path.join(__dirname, 'views', 'about.html'));
+// });
+
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server running on port", port);
